69efeba48b89642e60524348d5e94cd9a6e1ac55
Badreddine <2172646@cegepmontpetit.ca>
2022-08-30 17:31:44 -0400

    Exercice_Web

---
+++ Exercice semaine 01/src/main/java/org/example/org/Smihy/Exercice_Web.java
@@ -0,0 +1,20 @@
+package org.example.org.Smihy;
+
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Exercice_Web
+{
+    public static void main(String[] args)
+    {
+        try {
+            String text = new Scanner(new URL(args[0].toString()).openStream(), "UTF-8").useDelimiter("\\A").next();
+            System.out.println(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
