20fe34fa0c6013169503e5ec31f7bdd59b25cff2
Badreddine Smihy <2172646@cegepmontpetit.ca>
2022-10-06 18:23:21 -0400

    Dernier essaie avant l'examen

--- Exercice semaine 01/src/main/java/Semaine04/TriSimple.java
+++ Exercice semaine 01/src/main/java/Semaine04/TriSimple.java
@@ -1,11 +1,26 @@
 package Semaine04;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class TriSimple {
     public static void main(String[] args) {
+        // Creating an array list
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add elements
+        numbers.add(4);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println("Unsorted ArrayList: " + numbers);
+
+        // Using the sort() method
+        Collections.sort(numbers);
+        System.out.println("Sorted ArrayList: " + numbers);
+
+        numbers.sort(Collections.reverseOrder());
+        System.out.println("Sorted Reverse ArrayList: " + numbers);
+
+
     }
 
 }
